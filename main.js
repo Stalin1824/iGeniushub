6ceffdbc76e6c2f3e462d709c330d9f8c932e401
Stalin1824 <name.stalin2003@gmail.com>
2024-07-29 22:46:07 +0530

    update js

--- main.js
+++ main.js
@@ -1,26 +1,22 @@
-var toLeft = document.querySelector(".head-wrapper>div:nth-child(2)")  
+var toLeft = document.querySelector(".head-wrapper>div:nth-child(2)")
 var menuBar = document.querySelector(".head-wrapper>div:nth-child(3)")
 var section = document.querySelectorAll(".head-wrapper>div:nth-child(2) >a")
 
 const mediaQuery = window.matchMedia('(max-width: 880px)');
-let a = false
+
 
 // JS to Change MediaQuery  
 
 function handleMediaQuery(e) {
-  
+
 
   if (e.matches === true) {
     menuBar.addEventListener("click", () => {
-      a = !a
-      if (a === true) {
-        toLeft.style.left = "0%"
-        toLeft.style.transition = "1s"
-      } else if (a === false) {
-        toLeft.style.left = "-60%"
-        toLeft.style.transition = "1s"
-      }
-       
+
+      toLeft.style.left = "0%"
+      toLeft.style.transition = "1s"
+
+
     })
 
 
