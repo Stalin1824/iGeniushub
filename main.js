cd746c57d67ea33578cc970b237a5c84df6f85a1
Stalin1824 <name.stalin2003@gmail.com>
2024-07-29 22:03:51 +0530

    update js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var toLeft = document.querySelector(".head-wrapper>div:nth-child(2)")
+var toLeft = document.querySelector(".head-wrapper>div:nth-child(2)")  
 var menuBar = document.querySelector(".head-wrapper>div:nth-child(3)")
 var section = document.querySelectorAll(".head-wrapper>div:nth-child(2) >a")
 
